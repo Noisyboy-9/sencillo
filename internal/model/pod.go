ac0aea7a9107d0db5b48a15cb72ce058f24aa0bd
sina shariati <sina.shariati@yahoo.com>
2023-10-27 15:22:47 +0330

    Add id to pod struct

--- internal/model/pod.go
+++ internal/model/pod.go
@@ -5,13 +5,15 @@ import (
 )
 
 type Pod struct {
+	id         int
 	status     enum.PodStatus
 	node       *Node
 	deployment *Deployment
 }
 
-func NewPod(deployment *Deployment) *Pod {
+func NewPod(id int, deployment *Deployment) *Pod {
 	return &Pod{
+		id:         id,
 		status:     enum.PodStatusPendding,
 		node:       nil,
 		deployment: deployment,
@@ -21,8 +23,13 @@ func NewPod(deployment *Deployment) *Pod {
 func (pod *Pod) SetNode(node *Node) {
 	pod.node = node
 }
+
 func (pod *Pod) SetStatus(newStatus enum.PodStatus) {
 	pod.status = newStatus
+}
+
+func (pod *Pod) Id() int {
+	return pod.id
 }
 
 func (pod *Pod) Status() enum.PodStatus {
