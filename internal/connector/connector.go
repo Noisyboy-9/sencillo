44e576e4ea94537c51d2d7aaad50d02504926624
sina shariati <sina.shariati@yahoo.com>
2023-10-27 17:41:57 +0330

    Finish implementing bind and emit scheduling event functions

--- internal/connector/connector.go
+++ internal/connector/connector.go
@@ -1,7 +1,16 @@
 package connector
 
 import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/noisyboy-9/random-k8s-scheduler/internal/config"
 	"github.com/noisyboy-9/random-k8s-scheduler/internal/log"
+	"github.com/noisyboy-9/random-k8s-scheduler/internal/model"
+	v1 "k8s.io/api/core/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
@@ -36,4 +45,52 @@ func Connect() {
 
 func (connector *connector) Client() *kubernetes.Clientset {
 	return connector.client
+}
+
+func (connector *connector) BindPodToNode(pod *model.Pod, selectedNode *model.Node) error {
+	return ClusterConnection.Client().CoreV1().Pods(pod.Namespace()).Bind(
+		context.Background(),
+		&v1.Binding{
+			ObjectMeta: metaV1.ObjectMeta{
+				Name:      pod.Name(),
+				Namespace: pod.Namespace(),
+			},
+			Target: v1.ObjectReference{
+				APIVersion: "v1",
+				Kind:       "node",
+				Name:       selectedNode.Name(),
+			},
+		},
+		metaV1.CreateOptions{},
+	)
+
+}
+
+func (connector *connector) EmitScheduledEvent(pod *model.Pod, node *model.Node) error {
+	timestamp := time.Now().UTC()
+	_, err := ClusterConnection.Client().CoreV1().Events(pod.Namespace()).Create(
+		context.Background(),
+		&v1.Event{
+			Count:          1,
+			Message:        fmt.Sprintf("pod: %s has been bound to node: %s", pod.Name(), node.Name()),
+			Reason:         "Scheduled",
+			LastTimestamp:  metaV1.NewTime(timestamp),
+			FirstTimestamp: metaV1.NewTime(timestamp),
+			Type:           "Normal",
+			Source: v1.EventSource{
+				Component: config.Scheduler.Name,
+			},
+			InvolvedObject: v1.ObjectReference{
+				Kind:      "Pod",
+				Name:      pod.Name(),
+				Namespace: pod.Namespace(),
+				UID:       types.UID(pod.Id()),
+			},
+			ObjectMeta: metaV1.ObjectMeta{
+				GenerateName: pod.Name() + "-",
+			},
+		},
+		metaV1.CreateOptions{},
+	)
+	return err
 }
