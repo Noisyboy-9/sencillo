d4728937f401b7c0bb918a690b575f3460f472a2
sina shariati <sina.shariati@yahoo.com>
2023-11-01 20:22:44 +0330

    Add default tags to config.Scheduler

--- internal/config/scheduler.go
+++ internal/config/scheduler.go
@@ -1,8 +1,8 @@
 package config
 
 type scheduler struct {
-	Name      string
-	Namespace string
+	Name      string `json:"name,omitempty" default:"name"`
+	Namespace string `json:"namespace,omitempty" default:"namespace"`
 }
 
 var Scheduler *scheduler
