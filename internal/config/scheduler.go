d8bf0c7a997de917dcd62483a8f65c6ff9ac7039
sina shariati <sina.shariati@yahoo.com>
2024-03-18 12:43:06 +0330

    Feat: Add algorithm to config.Scheduler

--- internal/config/scheduler.go
+++ internal/config/scheduler.go
@@ -3,6 +3,7 @@ package config
 type scheduler struct {
 	Name      string `json:"name,omitempty" default:"name"`
 	Namespace string `json:"namespace,omitempty" default:"namespace"`
+	Algorithm string `json:"algorithm,omitempty" default:"algorithm"`
 }
 
 var Scheduler *scheduler
