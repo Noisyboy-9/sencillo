3b76e5fd4f9d4422286ab0450eea76d42121be93
sina shariati <sina.shariati@yahoo.com>
2024-03-21 14:02:50 +0330

    Fix: fix bug in cloud-first scheduler

--- internal/scheduler/cloud-first/cloud-first.go
+++ internal/scheduler/cloud-first/cloud-first.go
@@ -2,49 +2,42 @@ package cloud_first
 
 import (
 	"errors"
-	"math/rand"
-
+	"github.com/noisyboy-9/random-k8s-scheduler/internal/log"
 	"github.com/noisyboy-9/random-k8s-scheduler/internal/model"
+	"math/rand"
 )
 
 type CloudFirstScheduler struct{}
 
 func (c CloudFirstScheduler) Run(pod *model.Pod, nodes []*model.Node) (node *model.Node, err error) {
-	eligibleNodes := c.Filter(pod, nodes)
-	if len(eligibleNodes) == 0 {
+	edgeNodes, cloudNodes := c.Filter(pod, nodes)
+	if len(edgeNodes) == 0 && len(cloudNodes) == 0 {
 		return nil, errors.New("no eligible nodes found")
-	}
-	return c.Schedule(eligibleNodes)
-}
-func (c CloudFirstScheduler) Filter(pod *model.Pod, nodes []*model.Node) []*model.Node {
-	eligibleNodes := make([]*model.Node, 0)
-	for _, node := range nodes {
-		if node.IsOnEdge() && node.HasEnoughResourcesForPod(pod) {
-			eligibleNodes = append(eligibleNodes, node)
-		}
-	}
-	return eligibleNodes
-}
-func (c CloudFirstScheduler) Schedule(nodes []*model.Node) (node *model.Node, err error) {
-	edgeNodes := make([]*model.Node, 0)
-	cloudNodes := make([]*model.Node, 0)
 
+	}
+	return c.Schedule(edgeNodes, cloudNodes), nil
+}
+func (c CloudFirstScheduler) Filter(pod *model.Pod, nodes []*model.Node) (eligibleEdgeNodes []*model.Node, eligibleCloudNodes []*model.Node) {
+	eligibleEdgeNodes = make([]*model.Node, 0)
+	eligibleCloudNodes = make([]*model.Node, 0)
 	for _, node := range nodes {
-		if node.IsOnEdge() {
-			edgeNodes = append(edgeNodes, node)
-		} else {
-			cloudNodes = append(cloudNodes, node)
+		if node.HasEnoughResourcesForPod(pod) && node.IsOnEdge() {
+			eligibleEdgeNodes = append(eligibleEdgeNodes, node)
+		}
+
+		if node.HasEnoughResourcesForPod(pod) && !node.IsOnEdge() {
+			eligibleCloudNodes = append(eligibleCloudNodes, node)
 		}
 	}
 
-	if len(cloudNodes) >= 0 {
-		//	there is some cloud nodes that are eligible, select one of them randomly
-		return cloudNodes[rand.Intn(len(cloudNodes))], nil
+	return eligibleEdgeNodes, eligibleCloudNodes
+}
+
+func (c CloudFirstScheduler) Schedule(edgeNodes []*model.Node, cloudNodes []*model.Node) (node *model.Node) {
+	if len(cloudNodes) != 0 {
+		return cloudNodes[rand.Intn(len(cloudNodes))]
 	}
 
-	if len(edgeNodes) >= 0 {
-		return edgeNodes[rand.Intn(len(edgeNodes))], nil
-	}
-
-	return nil, errors.New("no eligible nodes")
+	log.App.Info("no cloud nodes were eligible, scheduling on random edge node ...")
+	return edgeNodes[rand.Intn(len(edgeNodes))]
 }
