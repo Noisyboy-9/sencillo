386833c81dbf92aceaee79ebb98f62a45d2e8b4c
sina shariati <sina.shariati@yahoo.com>
2024-03-27 10:12:22 +0330

    Feat: remove state and node from model.Pods

--- internal/model/pod.go
+++ internal/model/pod.go
@@ -1,15 +1,12 @@
 package model
 
 import (
-	"github.com/noisyboy-9/random-k8s-scheduler/internal/enum"
 	"k8s.io/apimachinery/pkg/api/resource"
 )
 
 type Pod struct {
 	id        string
 	name      string
-	status    enum.PodStatus
-	node      *Node
 	namespace string
 
 	cpu    *resource.Quantity
@@ -20,8 +17,6 @@ func NewPod(id, name, namespace string, cpu, memory *resource.Quantity) *Pod {
 	return &Pod{
 		id:        id,
 		name:      name,
-		status:    enum.PodStatusPendding,
-		node:      nil,
 		namespace: namespace,
 		cpu:       cpu,
 		memory:    memory,
@@ -31,28 +26,19 @@ func NewPod(id, name, namespace string, cpu, memory *resource.Quantity) *Pod {
 func (pod *Pod) Id() string {
 	return pod.id
 }
-func (pod *Pod) Status() enum.PodStatus {
-	return pod.status
-}
-func (pod *Pod) Node() *Node {
-	return pod.node
-}
+
 func (pod *Pod) Namespace() string {
 	return pod.namespace
 }
+
 func (pod *Pod) Name() string {
 	return pod.name
 }
+
 func (pod *Pod) Memory() *resource.Quantity {
 	return pod.memory
 }
-func (pod *Pod) Cpu() *resource.Quantity {
-	return pod.cpu
-}
 
-func (pod *Pod) SetStatus(newStatus enum.PodStatus) {
-	pod.status = newStatus
-}
-func (pod *Pod) SetNode(node *Node) {
-	pod.node = node
+func (pod *Pod) Cores() *resource.Quantity {
+	return pod.cpu
 }
