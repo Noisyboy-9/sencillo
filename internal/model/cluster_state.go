193dbc9a5a2d26d700a409f90346a790e6bf2f56
sina shariati <sina.shariati@yahoo.com>
2024-03-28 11:39:15 +0330

    Feat: make ClusterState thread safe

--- internal/model/cluster_state.go
+++ internal/model/cluster_state.go
@@ -46,54 +46,83 @@ func (s *ClusterState) SetIsNodesSynced(isNodesSynced bool) {
 }
 
 func (s *ClusterState) AddPod(p Pod) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
 	s.pods[p.ID] = p
 }
 
 func (s *ClusterState) RemovePod(p Pod) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
 	delete(s.pods, p.ID)
 }
 func (s *ClusterState) RemovePodByID(id types.UID) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
 	delete(s.pods, id)
 }
 
 func (s *ClusterState) EditPodWithUID(id types.UID, edited Pod) error {
+	s.mux.RLock()
 	if _, ok := s.pods[id]; !ok {
 		return errors.New("pod with given uid doesn't exist")
 	}
+	s.mux.RUnlock()
+
+	s.mux.Lock()
+	defer s.mux.Unlock()
 	s.pods[id] = edited
 	return nil
 }
 
 func (s *ClusterState) GetPodByUID(id types.UID) (pod Pod, exists bool) {
+	s.mux.RLock()
+	defer s.mux.RUnlock()
 	pod, exists = s.pods[id]
 	return
 }
 
 func (s *ClusterState) AddNode(n Node) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
 	s.nodes[n.ID] = n
 }
 
 func (s *ClusterState) RemoveNode(n Node) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
 	delete(s.nodes, n.ID)
 }
 
 func (s *ClusterState) RemoveNodeByID(id types.UID) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
 	delete(s.nodes, id)
 }
 
 func (s *ClusterState) EditNodeWithUID(id types.UID, edited Node) error {
+	s.mux.RLock()
 	if _, ok := s.nodes[id]; !ok {
 		return errors.New("node with given id doesn't exist")
 	}
+	s.mux.RUnlock()
+
+	s.mux.Lock()
+	defer s.mux.Unlock()
 	s.nodes[id] = edited
 	return nil
 }
 func (s *ClusterState) GetNodeWithID(id types.UID) (node Node, exists bool) {
+	s.mux.RLock()
+	defer s.mux.RUnlock()
 	node, exists = s.nodes[id]
 	return
 }
 
 func (s *ClusterState) Nodes() []Node {
+	s.mux.RLock()
+	defer s.mux.RUnlock()
+
 	nodes := make([]Node, 0, len(s.nodes))
 	i := 0
 	for _, n := range s.nodes {
@@ -105,6 +134,9 @@ func (s *ClusterState) Nodes() []Node {
 }
 
 func (s *ClusterState) Pods() []Pod {
+	s.mux.RLock()
+	defer s.mux.RUnlock()
+
 	pods := make([]Pod, 0, len(s.pods))
 	i := 0
 	for _, p := range s.pods {
